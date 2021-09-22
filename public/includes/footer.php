1fc4fd81cec80e4272eb71e778f7af02f24ddd72
Felipe_Estevanatto <69355764+FelipeEstevanatto@users.noreply.github.com>
2021-09-22 08:50:43 -0300

    Update footer.php

--- public/includes/footer.php
+++ public/includes/footer.php
@@ -7,10 +7,10 @@
 
     <div class="content">
         <div class="left">
-            <a href="../../index.php" data-checked="home" class="btn">Home</a>
-            <a href="products.php" data-checked="products" class="btn">Produtos</a>
-            <a href="#Estatics" data-checked="estatics" class="btn">Estatísticas</a>
-            <a href="development.php" data-checked="development" class="btn">Desenvolvimento</a>
+            <a href="<?=URLROOT?>/" data-checked="home" class="btn">Home</a>
+            <a href="<?=URLROOT?>/public/views/products.php" data-checked="products" class="btn">Produtos</a>
+            <a href="<?=URLROOT?>/public/views/statistics.php" data-checked="statistics" class="btn">Estatísticas</a>
+            <a href="<?=URLROOT?>/public/views/development.php" data-checked="development" class="btn">Desenvolvimento</a>
             <?php
                 if (isset($_SESSION['isAuth'])) {
                     echo '<a href="'.URLROOT.'/public/views/user.php" data-checked="register" class="btn primary">Perfil</a>';

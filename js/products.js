ad42d1b7f5adb93279ab5f5d857cfac09c564e52
Gabriel Nicolim <gabriel.gomes.nicolim@gmail.com>
2021-11-06 13:50:29 -0300

    cart update
    Co-Authored-By: Felipe_Estevanatto <69355764+FelipeEstevanatto@users.noreply.github.com>

--- js/products.js
+++ js/products.js
@@ -23,6 +23,7 @@ window.addEventListener('load', () => {
         let id_product = window.document.querySelectorAll('#products .product');
         let add_cart = window.document.querySelectorAll('#products .product .to-cart');
         let cart_icon = window.document.querySelector('.shop-car a span');
+        let cart_items = [];
 
         add_cart.forEach((element, key) => {
             element.addEventListener('click', () => {
@@ -31,10 +32,13 @@ window.addEventListener('load', () => {
                 xhr.open("POST", "../../app/manageCart.php");
                 xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
                 xhr.onload = (e) => {
-                     if (xhr.readyState === 4 && xhr.status === 200) {
-                         if (cart_icon.innerHTML == '') cart_icon.innerHTML = 1;
-                         else cart_icon.innerHTML = parseInt(cart_icon.innerHTML) + 1;
-                     }
+                    if (xhr.readyState === 4 && xhr.status === 200) {
+                        if (!(cart_items.indexOf(id_product[key].id) != -1)) {
+                            if (cart_icon.innerHTML == '') cart_icon.innerHTML = 1;
+                            else cart_icon.innerHTML = parseInt(cart_icon.innerHTML) + 1;
+                            cart_items.push(id_product[key].id);
+                        }
+                    }
                 };
                 xhr.send('id='+id_product[key].id + '&add=1');
             })
